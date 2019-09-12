6b8c6718c1f2e7d639e2c0adf1dca3b982a0fd30
Razvan Popa <razvancristianpopa@gmail.com>
2019-09-12 10:59:05 +0300

    2 - Header v1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import './resources/styles.css';
+import React from "react";
+import "./resources/styles.css";
+
+import Header from "./components/header_footer/Header";
+// import Footer from "./components/header_footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div
+      className="App"
+      style={{ height: "1500px", backgroundColor: "purple" }}
+    >
+      <Header />
     </div>
   );
 }
