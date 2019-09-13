ece73413ba6c30e8e50baeb80361cce8ea9160a2
Razvan Cristian Popa <razluc1@gmail.com>
2019-09-13 14:41:22 +0300

    6 - Event Info section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from "react";
-import "./resources/styles.css";
+import React from 'react';
+import './resources/styles.css';
 
-import Header from "./components/header_footer/Header";
-import Featured from "./components/featured";
+import Header from './components/header_footer/Header';
+import Featured from './components/featured';
+import VenueNfo from './components/venueNfo';
 // import Footer from "./components/header_footer/Footer";
 
 function App() {
   return (
     <div
       className="App"
-      style={{ height: "1500px", backgroundColor: "purple" }}
+      style={{ height: '1500px', backgroundColor: 'purple' }}
     >
       <Header />
       <Featured />
+      <VenueNfo />
     </div>
   );
 }
